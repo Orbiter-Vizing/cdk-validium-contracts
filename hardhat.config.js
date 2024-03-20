0384e670cb162da22b84b556b34deb0b830becd5
Pis <piscarefree@gmail.com>
2024-03-20 16:04:03 +0800

    format code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,19 @@
-require('dotenv').config();
-require('@nomiclabs/hardhat-waffle');
-require('hardhat-gas-reporter');
-require('solidity-coverage');
-require('@nomiclabs/hardhat-etherscan');
-require('@openzeppelin/hardhat-upgrades');
-require('hardhat-dependency-compiler');
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
+require("hardhat-dependency-compiler");
 
 const { ProxyAgent, setGlobalDispatcher } = require("undici");
 const proxyAgent = new ProxyAgent("http://127.0.0.1:7890");
-setGlobalDispatcher(proxyAgent);
+if (!!process.env.PROXYAGENT) {
+  setGlobalDispatcher(proxyAgent);
+}
 
-const DEFAULT_MNEMONIC = 'test test test test test test test test test test test junk';
+const DEFAULT_MNEMONIC =
+  "test test test test test test test test test test test junk";
 
 /*
  * You need to export an object to set up your config
@@ -23,10 +26,10 @@ const DEFAULT_MNEMONIC = 'test test test test test test test test test test test
 module.exports = {
   dependencyCompiler: {
     paths: [
-      '@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol',
-      '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol',
-      '@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol'
-    ]//,
+      "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol",
+      "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol",
+      "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol",
+    ], //,
     //keep: true
   },
   solidity: {
@@ -34,52 +37,47 @@ module.exports = {
       {
         version: "0.8.20",
         settings: {
-          evmVersion: 'paris',
+          evmVersion: "paris",
           optimizer: {
             enabled: true,
-            runs: 999999
-          }
-        }
+            runs: 999999,
+          },
+        },
       },
       {
         version: "0.6.11",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999
-          }
-        }
+            runs: 999999,
+          },
+        },
       },
       {
         version: "0.5.12",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999
-          }
-        }
+            runs: 999999,
+          },
+        },
       },
       {
         version: "0.5.16",
         settings: {
           optimizer: {
             enabled: true,
-            runs: 999999
-          }
-        }
-      }
-    ]
+            runs: 999999,
+          },
+        },
+      },
+    ],
   },
   networks: {
-    // mainnet: {
-    //   url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-    //   accounts: {
-    //     mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
-    //     path: "m/44'/60'/0'/0",
-    //     initialIndex: 0,
-    //     count: 20,
-    //   },
-    // },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`${process.env.DEPLOYER_PRIVATE_KEY}`],
+    },
     ropsten: {
       url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: {
@@ -125,7 +123,7 @@ module.exports = {
         count: 20,
       },
     },
-    zkfair:{
+    zkfair: {
       url: process.env.ZKFAIR_URL,
       accounts: {
         mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
@@ -134,7 +132,7 @@ module.exports = {
         count: 20,
       },
     },
-    cadona:{
+    cadona: {
       url: process.env.CARDONA,
       accounts: {
         mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
@@ -143,7 +141,7 @@ module.exports = {
         count: 20,
       },
     },
-    polygon_zkevm:{
+    polygon_zkevm: {
       url: process.env.POLYGON_ZKEVM,
       accounts: {
         mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
@@ -153,7 +151,7 @@ module.exports = {
       },
     },
     localhost: {
-      url: 'http://127.0.0.1:8545',
+      url: "http://127.0.0.1:8545",
       accounts: {
         mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
         path: "m/44'/60'/0'/0",
@@ -162,7 +160,7 @@ module.exports = {
       },
     },
     hardhat: {
-      initialDate: '0',
+      initialDate: "0",
       allowUnlimitedContractSize: true,
       accounts: {
         mnemonic: process.env.MNEMONIC || DEFAULT_MNEMONIC,
@@ -170,10 +168,6 @@ module.exports = {
         initialIndex: 0,
         count: 20,
       },
-    },
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`${process.env.DEPLOYER_PRIVATE_KEY}`]
     },
   },
   gasReporter: {
@@ -186,7 +180,7 @@ module.exports = {
     apiKey: {
       goerli: `${process.env.ETHERSCAN_API_KEY}`,
       sepolia: `${process.env.ETHERSCAN_API_KEY}`,
-      mainnet: `${process.env.ETHERSCAN_API_KEY}`
+      mainnet: `${process.env.ETHERSCAN_API_KEY}`,
     },
   },
 };
